0aae06156b16ccd596dacee306bbb0035cbce382
shikatetteh <shikatet30@gmail.com>
2020-02-06 23:46:51 +0000

    tabs setup

---
+++ screens/InboxScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const InboxScreen = () => {
+  return (
+    <View>
+      <Text>Inbox</Text>
+    </View>
+  );
+};
+
+export default InboxScreen;
